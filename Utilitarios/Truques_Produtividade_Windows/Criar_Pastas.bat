1d90b42e2b943e054392f4296dbad81defe917a5
Inajara <inajaralemos1@gmail.com>
2025-05-26 17:12:18 -0300

    Separação de conteúdo

--- Utilitarios/Truques_Produtividade_Windows/Criar_Pastas.bat
+++
@@ -1,5 +0,0 @@
-REM Este é um script para criar n-pastas
-REM Basta renomar as pastas de acordo com o padrão que se queira
-REM Exmplo: pasta1, pasta2, ...
-
-MD Wave_1 Wave_2 Wave_3 Wave_4 Wave_5 Wave_6 Wave_7 Wave_8 Wave_9 Wave_10 Wave_11
